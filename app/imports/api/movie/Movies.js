b5d4d697c20a3b03baaafc588a76e86261990b9a
maeganmc <maeganmc@users.noreply.github.com>
2021-06-14 01:16:16 -0400

    ratings

--- app/imports/api/movie/Movies.js
+++ app/imports/api/movie/Movies.js
@@ -16,6 +16,7 @@ class MoviesCollection {
       title: String,
       synopsis: String,
       image: String,
+      rating: String,
     }, { tracker: Tracker });
     // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
     this.collection.attachSchema(this.schema);
--- app/imports/ui/components/Movie.jsx
+++ app/imports/ui/components/Movie.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { _ } from 'meteor/underscore';
-import { Card, Image, Header, Button } from 'semantic-ui-react';
+import { Card, Image, Header, Button, Rating, Feed, CardContent } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { NavLink, withRouter } from 'react-router-dom';
 import GenreLabel from './GenreLabel';
@@ -18,12 +18,23 @@ class Movie extends React.Component {
             src={this.props.movie.image} fluid
           />
           <Card.Header>
-            <h2 className="ui center aligned header">{this.props.movie.title}</h2></Card.Header>
+            <div style={{ paddingTop: '10px' }}>
+              <h2 className="ui center aligned header">{this.props.movie.title}</h2></div></Card.Header>
           <Card.Description>Synopsis: {this.props.movie.synopsis}</Card.Description>
           <Header as='h4'>Genres:</Header>
           {alphaSort(this.props.movie_genres).map((movie_genre, index) => <GenreLabel key={index} movie_genre={movie_genre}/>)}
         </Card.Content>
         <br/>
+        <Card.Content extra>
+          <Feed.Event>
+            <Feed.Content>
+              <Feed.Summary>
+                <div style={{ paddingLeft: '75px' }}>
+                  <Rating fluid icon='heart' maxRating={5} defaultRating={this.props.movie.rating}/></div>
+              </Feed.Summary>
+            </Feed.Content>
+          </Feed.Event>
+        </Card.Content>
         <Card.Header>
           <Header textAlign='center'>
             <Button fluid size='tiny' color='black' as={NavLink} exact to={`/course/${this.props.movie._id}`}>Review</Button>
